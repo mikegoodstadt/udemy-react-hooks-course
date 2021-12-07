c1e57f45d6ffc358fffe704310d92c81e3789708
Mike Goodstadt <mike.goodstadt@gmail.com>
2021-12-07 09:18:15 +0100

    getProfile from GitHub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 
+const initProfile = {
+  name: null,
+  followers: null,
+  publicRepos: null
+}
+
 function App() {
+  const [profile, setProfile] = useState(initProfile);
+
+  async function getProfile(name) {
+    const response = await fetch(`http://api.github.com/users/${name}`);
+    const json = await response.json();
+
+    setProfile({
+      name: json.login,
+      followers: json.followers,
+      publicRepos: json.public_repos
+    })
+  }
+
+  useEffect(() => {
+    getProfile('mikegoodstadt')
+  })
+
   return (
     <div className='App'>
       <header className='App-header'>
+        <h2>Fetch Data for: {profile.name}</h2>
+        <h3>{`Followers: ${profile.followers}`}</h3>
+        <h3>{`Pubic Repos: ${profile.publicRepos}`}</h3>
       </header>
     </div>
   );
