d34eb1151d298c80730ded8182a58632ca46291e
Mike Goodstadt <mike.goodstadt@gmail.com>
2021-12-07 12:10:38 +0100

    useDebugValue in React Dev Tools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import usePrevious from './hook/usePrevious';
 
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-      <h2>Current Age: {age}</h2>
-      <h2>Previous Age: {previousAge}</h2>
-      <button onClick={() => setAge(age - 1)}>Make me Young</button>
+        <h2>Current Age: {age}</h2>
+        <h2>Previous Age: {previousAge}</h2>
+        <button onClick={() => setAge(age - 1)}>Make me Young</button>
       </header>
     </div>
   );
--- src/hook/usePrevious.js
+++ src/hook/usePrevious.js
@@ -1,10 +1,13 @@
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef, useDebugValue } from 'react';
 
 function usePrevious(value) {
   const ref = useRef(null);
   useEffect(() => {
     ref.current = value;
-  })
+  });
+
+  useDebugValue(ref.current > 18 ? 'too much' : 'too little');
+
   return ref.current;
 }
 
