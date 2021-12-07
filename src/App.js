9cde25e7783ad64ab6a320af6000e0a0ab2cd0ea
Mike Goodstadt <mike.goodstadt@gmail.com>
2021-12-07 09:47:17 +0100

    previous age

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
+import usePrevious from './hook/usePrevious';
 
 function App() {
+  const [age, setAge] = useState(21);
+  const previousAge = usePrevious(age);
+
   return (
     <div className='App'>
       <header className='App-header'>
+      <h2>Current Age: {age}</h2>
+      <h2>Previous Age: {previousAge}</h2>
+      <button onClick={() => setAge(age - 1)}>Make me Young</button>
       </header>
     </div>
   );
