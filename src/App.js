89e98c973cc0b6c5aa13bd6bba7e9288d429af67
Mike Goodstadt <mike.goodstadt@gmail.com>
2021-12-10 08:31:28 +0100

    7.49 unit test; snapshot test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Button from './components/button/button';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <Button label='click me please'></Button>
       </header>
     </div>
   );
